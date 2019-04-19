1ddb0ad5ab6babeb28bb06dacb1904d331ef69a3
Zhi Li <lizhi1977@hotmail.com>
2019-04-19 09:53:38 +0000

    add stream helper class for parse the multi part form.

--- Request.h
+++ Request.h
@@ -5,7 +5,9 @@
 #include <vector>
 #include <sstream>
 
-#include "/usr/local/include/fcgio.h"
+#include <fcgio.h>
+
+#include "RequestStream.h"
 
 using namespace std;
 
@@ -15,306 +17,294 @@ class Request
 {
   friend class ControllerBase;
 
-  private:
-    fcgi_streambuf m_cout_fcgi_streambuf;
-    ostream m_os;
-    map<string, string> m_params;
-    vector<string> m_urlPartials;
-    string nullstr;
-    string m_boundary;
+private:
+  fcgi_streambuf m_cout_fcgi_streambuf;
+  ostream m_os;
+  map<string, string> m_params;
+  vector<string> m_urlPartials;
+  string nullstr;
+  string m_boundary;
 
-    void SplitParams(const string data)
+  void SplitParams(const string data)
+  {
+    string::size_type pos = 0;
+
+    while (pos != data.length())
     {
-      string::size_type pos = 0;
-
-      while(pos != data.length())
+      auto idx = data.find('&', pos);
+      if (idx != string::npos)
       {
-        auto idx = data.find('&', pos);
-        if(idx != string::npos)
-        {
-          // found the &!!
-          // get the substring before &;
-          AddParam(data.substr(pos, idx-pos));
-          pos = idx + 1;
-        }
-        else
-        {
-          // no & in the string.
-          // all the string will be parsed as one parameter
-          AddParam(data.substr(pos));
-          break;
-        }
+        // found the &!!
+        // get the substring before &;
+        AddParam(data.substr(pos, idx - pos));
+        pos = idx + 1;
+      }
+      else
+      {
+        // no & in the string.
+        // all the string will be parsed as one parameter
+        AddParam(data.substr(pos));
+        break;
       }
     }
+  }
 
-    void AddParam(const string & str)
+  void AddParam(const string & str)
+  {
+    auto idx = str.find('=');
+    if (idx != string::npos)
     {
-      auto idx = str.find('=');
-      if(idx != string::npos)
+      // found the =!!
+      // get the substring before = as param name and 
+      // after = is the part of value.
+      string key(move(HttpStrDecode(str.substr(0, idx))));
+      if (m_params.find(key) == m_params.end())
       {
-        // found the =!!
-        // get the substring before = as param name and 
-        // after = is the part of value.
-        string key( move ( HttpStrDecode ( str.substr(0, idx) ) ) );
-        if ( m_params.find(key) == m_params.end() )
-        {
-          m_params.insert(make_pair(key, HttpStrDecode(str.substr(idx+1))));
-        } 
+        m_params.insert(make_pair(key, HttpStrDecode(str.substr(idx + 1))));
       }
     }
+  }
 
-    /*
-     * Decode the http request string using utf8.
-     */
-    string HttpStrDecode(const string& data)
+  /*
+   * Decode the http request string using utf8.
+   */
+  string HttpStrDecode(const string & data)
+  {
+    ostringstream os;
+    string::size_type pos = 0;
+
+    while (pos != data.length())
     {
-      ostringstream os;
-      string::size_type pos = 0;
-
-      while(pos != data.length())
+      auto idx = data.find('%', pos);
+      if (idx != string::npos)
       {
-        auto idx = data.find('%', pos);
-        if(idx != string::npos)
-        {
-          // found!!
-          os << data.substr(pos, idx-pos);
-          // push the following 2 char as char code into the string stream.
-          os << static_cast<char>(stoi(data.substr(idx+1, 2), nullptr, 16));
-          pos = idx + 3;
-        }
-        else
-        {
-          // no % in the last string.
-          os << data.substr(pos);
-          pos = data.length();
-        }
+        // found!!
+        os << data.substr(pos, idx - pos);
+        // push the following 2 char as char code into the string stream.
+        os << static_cast<char>(stoi(data.substr(idx + 1, 2), nullptr, 16));
+        pos = idx + 3;
       }
-
-      os << flush;
-
-      return os.str();
+      else
+      {
+        // no % in the last string.
+        os << data.substr(pos);
+        pos = data.length();
+      }
     }
 
-    /*
-     * in the request uri like "/fcgi/soname/action/..."
-     * find the segment following the /fcgi/
-     * it will be used as share object name and the action name.
-     */
-    void GetUrlPartials(const string& requestUri)
+    os << flush;
+
+    return os.str();
+  }
+
+  /*
+   * in the request uri like "/fcgi/soname/action/..."
+   * find the segment following the /fcgi/
+   * it will be used as share object name and the action name.
+   */
+  void GetUrlPartials(const string & requestUri)
+  {
+    m_urlPartials.clear();
+
+    auto startPos = requestUri.find("/fcgi/", 0);
+    if (startPos != string::npos)
     {
-      m_urlPartials.clear();
-
-      auto startPos = requestUri.find("/fcgi/", 0);
-      if(startPos != string::npos)
+      startPos += 6;
+      auto endPos = requestUri.find('/', startPos);
+      while (endPos != string::npos)
       {
-        startPos += 6;
-        auto endPos = requestUri.find('/', startPos);
-        while(endPos != string::npos)
-        {
-          m_urlPartials.push_back(requestUri.substr(startPos, endPos-startPos));
-          startPos = endPos + 1;
-          endPos = requestUri.find('/', startPos);
-        }
+        m_urlPartials.push_back(requestUri.substr(startPos, endPos - startPos));
+        startPos = endPos + 1;
+        endPos = requestUri.find('/', startPos);
+      }
 
-        if (startPos != string::npos)
-        {
-          m_urlPartials.push_back(requestUri.substr(startPos, endPos-startPos));
+      if (startPos != string::npos)
+      {
+        m_urlPartials.push_back(requestUri.substr(startPos, endPos - startPos));
+      }
+    }
+  }
+
+  void ReadBoundary(const string & contentType) {
+    auto pos = contentType.find("boundary=", 0);
+    if (pos != string::npos) {
+      pos += 9;
+      m_boundary = "--";
+      m_boundary += contentType.substr(pos);
+    }
+  }
+
+  int searchInBuf(const char* buf, int startIdx, int endIdx, const char* key) {
+    auto keyLen = strlen(key);
+    int searchIdx = 0;
+    for (int i = startIdx; i <= endIdx; i++) {
+      if (buf[i] == key[searchIdx]) {
+        searchIdx++;
+        if (searchIdx >= keyLen) {
+          return i;
         }
+      }
+      else {
+        searchIdx = 0;
       }
     }
 
-    void ReadBoundary(const string& contentType){
-      auto pos = contentType.find("boundary=", 0);
-      if(pos != string::npos){
-        pos += 9;
-        m_boundary = "--";
-        m_boundary += contentType.substr(pos);
-      }
+    return -1;
+  }
+
+  string Parse(const char* multiContents, int startIdx, int endIdx, const char* key) {
+    int keyLen = strlen(key);
+    if (keyLen > 97) return nullstr;
+
+    char skey[100];
+    strcpy(skey, key);
+    strcpy(skey + keyLen, "=\"");
+    keyLen += 2;
+    skey[keyLen] = 0;
+
+    int idx = searchInBuf(multiContents, startIdx, endIdx, skey);
+    if (idx == -1) return nullstr;
+
+    while (multiContents[idx - keyLen] != ' ' && multiContents[idx - keyLen] != ';') {
+      idx = searchInBuf(multiContents, idx + 1, endIdx, skey);
+      if (idx == -1) return nullstr;
     }
 
-    int searchInBuf(const char * buf, int startIdx, int endIdx, const char * key){
-      auto keyLen = strlen(key);
-      int searchIdx = 0;
-      for(int i = startIdx; i <= endIdx; i++){
-        if(buf[i] == key[searchIdx]){
-          searchIdx ++;
-          if(searchIdx >= keyLen){
-            return i;
+    int idx2 = searchInBuf(multiContents, idx + 1, endIdx, "\"");
+    if (idx2 == -1) return nullstr;
+
+    string retval(multiContents + idx + 1, multiContents + idx2);
+    return retval;
+  }
+
+  void ParseParams(istream & fin, int contLen) {
+    RequestStream rs{ &fin,  &m_boundary, static_cast<size_t>(contLen) };
+    rs.SeekToNextSection();
+    string header;
+
+    while (rs.ReadHeader(header)) {
+
+      string name = Parse(header.c_str(), 0, header.size() - 1, "name");
+      string filename = Parse(header.c_str(), 0, header.size() - 1, "filename");
+      if (name != nullstr) {
+        if (filename == nullstr) {
+          // common input fields.
+          string key(move(HttpStrDecode(name)));
+          string val;
+          rs.ReadTextBody(val);
+          if (m_params.find(key) == m_params.end()) {
+            m_params.insert(make_pair(key, HttpStrDecode(val)));
           }
-        } else{
-          searchIdx = 0;
+        }
+        else {
+          // uploaded file.
         }
       }
 
-      return -1;
     }
+  }
 
-    string Parse(char * multiContents, int startIdx, int endIdx, const char * key){
-      int keyLen = strlen(key);
-      if(keyLen > 97) return nullstr;
+public:
+  string queryString, requestMethod, contentType, contentLength,
+    scriptName, requestUri, documentUri, documentRoot,
+    serverProtocol, gatewayInterface, serverSoftware,
+    remoteAddr, remotePort, serverAddr, serverPort,
+    serverName, postContents;
 
-      char skey[100];
-      strcpy(skey, key);
-      strcpy(skey + keyLen, "=\"");
-      keyLen += 2;
-      skey[keyLen] = 0;
+  Request(const FCGX_Request & request)
+    : m_cout_fcgi_streambuf(request.out),
+    m_os(&m_cout_fcgi_streambuf),
+    nullstr(""), m_boundary("")
+  {
+    // get raw params from environment viables
+    queryString = FCGX_GetParam("QUERY_STRING", request.envp);
+    requestMethod = FCGX_GetParam("REQUEST_METHOD", request.envp);
+    contentType = FCGX_GetParam("CONTENT_TYPE", request.envp);
+    contentLength = FCGX_GetParam("CONTENT_LENGTH", request.envp);
+    scriptName = FCGX_GetParam("SCRIPT_NAME", request.envp);
+    requestUri = FCGX_GetParam("REQUEST_URI", request.envp);
+    documentUri = FCGX_GetParam("DOCUMENT_URI", request.envp);
+    documentRoot = FCGX_GetParam("DOCUMENT_ROOT", request.envp);
+    serverProtocol = FCGX_GetParam("SERVER_PROTOCOL", request.envp);
+    gatewayInterface = FCGX_GetParam("GATEWAY_INTERFACE", request.envp);
+    serverSoftware = FCGX_GetParam("SERVER_SOFTWARE", request.envp);
+    remoteAddr = FCGX_GetParam("REMOTE_ADDR", request.envp);
+    remotePort = FCGX_GetParam("REMOTE_PORT", request.envp);
+    serverAddr = FCGX_GetParam("SERVER_ADDR", request.envp);
+    serverPort = FCGX_GetParam("SERVER_PORT", request.envp);
+    serverName = FCGX_GetParam("SERVER_NAME", request.envp);
 
-      int idx = searchInBuf(multiContents, startIdx, endIdx, skey);
-      if(idx == -1) return nullstr;
+    // analyse the query string (must be in the encoded status).
+    m_params.clear();
+    SplitParams(queryString);
 
-      while (multiContents[idx - keyLen] != ' ' && multiContents[idx - keyLen] != ';'){
-        idx = searchInBuf(multiContents, idx + 1, endIdx, skey);
-        if(idx == -1) return nullstr;
-      }
+    // read the post contents.
+    postContents = "";
+    if (contentLength.length() > 0)
+    {
+      int contLen = stoi(contentLength);
+      fcgi_streambuf cin_fcgi_streambuf(request.in);
+      istream fin(&cin_fcgi_streambuf);
 
-      int idx2 = searchInBuf(multiContents, idx + 1, endIdx, "\"");
-      if(idx2 == -1) return nullstr;
-
-      string retval(multiContents+idx+1, multiContents+idx2);
-      return retval;
-    }
-
-    void ParseParam(char * multiContents, int startIdx, int endIdx){
-      int idx = searchInBuf(multiContents, startIdx, endIdx, "\r\n\r\n");
-      if(idx >= 0){
-        string name = Parse(multiContents, startIdx, idx - 4, "name");
-        string filename = Parse(multiContents, startIdx, idx - 4, "filename");
-        if(name != nullstr){
-          if(filename == nullstr) {
-            // common input fields.
-            string key( move ( HttpStrDecode (name) ));
-            string val( multiContents + idx + 1, multiContents + endIdx);
-            if ( m_params.find(key) == m_params.end() )
-            {
-              m_params.insert(make_pair(key, HttpStrDecode(val)));
-            }
-          } else {
-            // uploaded file.
-          }
-          
-        }
-      }
-    }
-
-    void ParseParams(char * multiContents, int contLen){
-      auto boundaryLen = m_boundary.size();
-      int idx = 0;
-      int startIdx = 0;
-      int endIdx = 0;
-      while(1){
-        idx = searchInBuf(multiContents, idx, contLen-1, m_boundary.c_str());
-        if (idx == -1) break;
-        endIdx = idx - boundaryLen;
-        if(startIdx < endIdx) {
-          ParseParam(multiContents, startIdx, endIdx);
-        }
-        startIdx = idx + 1;
-      }
-    }
-
-  public:
-    string queryString, requestMethod, contentType, contentLength, 
-           scriptName, requestUri, documentUri, documentRoot, 
-           serverProtocol, gatewayInterface, serverSoftware, 
-           remoteAddr, remotePort, serverAddr, serverPort, 
-           serverName, postContents;
-
-    Request(const FCGX_Request &request)
-      : m_cout_fcgi_streambuf(request.out),
-        m_os(&m_cout_fcgi_streambuf),
-        nullstr(""), m_boundary("")
-    { 
-      // get raw params from environment viables
-      queryString = FCGX_GetParam("QUERY_STRING", request.envp);
-      requestMethod = FCGX_GetParam("REQUEST_METHOD", request.envp);
-      contentType = FCGX_GetParam("CONTENT_TYPE", request.envp);
-      contentLength = FCGX_GetParam("CONTENT_LENGTH", request.envp);
-      scriptName = FCGX_GetParam("SCRIPT_NAME", request.envp);
-      requestUri = FCGX_GetParam("REQUEST_URI", request.envp);
-      documentUri = FCGX_GetParam("DOCUMENT_URI", request.envp);
-      documentRoot = FCGX_GetParam("DOCUMENT_ROOT", request.envp);
-      serverProtocol = FCGX_GetParam("SERVER_PROTOCOL", request.envp);
-      gatewayInterface = FCGX_GetParam("GATEWAY_INTERFACE", request.envp);
-      serverSoftware = FCGX_GetParam("SERVER_SOFTWARE", request.envp);
-      remoteAddr = FCGX_GetParam("REMOTE_ADDR", request.envp);
-      remotePort = FCGX_GetParam("REMOTE_PORT", request.envp);
-      serverAddr = FCGX_GetParam("SERVER_ADDR", request.envp);
-      serverPort = FCGX_GetParam("SERVER_PORT", request.envp);
-      serverName = FCGX_GetParam("SERVER_NAME", request.envp);
-
-      // analyse the query string (must be in the encoded status).
-      m_params.clear();
-      SplitParams(queryString);
-       
-      // read the post contents.
-      postContents = "";
-      if(contentLength.length()>0)
-      {
-        int contLen = stoi(contentLength);
-        fcgi_streambuf cin_fcgi_streambuf(request.in);
-        istream fin(&cin_fcgi_streambuf);
-        string content;
-        char *buf = new char[contLen+1];
+      // analyse the post contents(must be in the encoded status).
+      if (contentType == "application/x-www-form-urlencoded") {
+        char* buf = new char[contLen + 1];
         fin.read(buf, contLen);
         buf[contLen] = 0;
         postContents = HttpStrDecode(buf);
-
-        // analyse the post contents(must be in the encoded status).
-        if(contentType == "application/x-www-form-urlencoded"){
-          SplitParams(buf);
+        SplitParams(buf);
+        delete[] buf;
+      }
+      else if (contentType.compare(0, 20, "multipart/form-data;") == 0) {
+        ReadBoundary(contentType);
+        if (m_boundary != "") {
+          ParseParams(fin, contLen);
         }
-        else if(contentType.compare(0, 20, "multipart/form-data;") == 0){
-          ReadBoundary(contentType);
-          if(m_boundary != ""){
-            ParseParams(buf, contLen);
-          }
-        }
-
-        delete [] buf;
       }
-
-      // get url partials.
-      GetUrlPartials(documentUri);
     }
 
-    const string& GetParam(const string& paramName) const
+    // get url partials.
+    GetUrlPartials(documentUri);
+  }
+
+  const string& GetParam(const string & paramName) const
+  {
+    try { return m_params.at(paramName); }
+    catch (...) { return nullstr; }
+  }
+
+  ostream& GetOutput()
+  {
+    return m_os;
+  }
+
+  const string& GetSoName() const
+  {
+    if (m_urlPartials.size() > 0)
     {
-      try{ return m_params.at(paramName); }
-      catch(...) { return nullstr; }
+      return m_urlPartials[0];
     }
 
-    ostream& GetOutput()
+    return nullstr;
+  }
+
+  const string& GetAction() const
+  {
+    if (m_urlPartials.size() > 1)
     {
-      return m_os;
+      return m_urlPartials[1];
     }
 
-    const string& GetSoName() const
+    return nullstr;
+  }
+
+  const string& GetUrlParams(int idx) const
+  {
+    if (m_urlPartials.size() > idx + 2)
     {
-      if(m_urlPartials.size() > 0)
-      {
-        return m_urlPartials[0];
-      }
-
-      return nullstr;
+      return m_urlPartials[idx + 2];
     }
 
-    const string& GetAction() const
-    {
-      if(m_urlPartials.size() > 1)
-      {
-        return m_urlPartials[1];
-      }
-
-      return nullstr;
-    }
-
-    const string& GetUrlParams(int idx) const
-    {
-      if(m_urlPartials.size() > idx+2)
-      {
-        return m_urlPartials[idx+2];
-      }
-
-      return nullstr;
-    }
+    return nullstr;
+  }
 };
---
+++ RequestStream.h
@@ -0,0 +1,136 @@
+#pragma once
+
+#include <istream>
+#include <string>
+#include <deque>
+
+using namespace std;
+class RequestStream {
+
+private:
+  istream* m_stream;
+  const string* m_boundary;
+  size_t m_length;
+  size_t m_readPtr;
+  deque<char> escbuf;
+
+public:
+  RequestStream(istream* stream, const string* boundary, size_t length) {
+    m_stream = stream;
+    m_boundary = boundary;
+    m_readPtr = 0;
+    m_length = length;
+  }
+
+private:
+  // found: return 1
+  // not found until the end of the stream: return 0
+  // and the read pointer is located at the end of the stream.
+  int SeekUntil(const string& key) {
+    auto keyLen = key.size();
+    int searchIdx = 0;
+    char c;
+    while (m_readPtr++ < m_length) {
+      m_stream->get(c);
+      if (c == key[searchIdx]) {
+        searchIdx++;
+        if (searchIdx >= keyLen) {
+          return 1;
+        }
+      }
+      else {
+        searchIdx = 0;
+      }
+    }
+
+    return 0;
+  }
+
+  bool ReadUntil(char* buf, size_t bufSize, const string& key, size_t& cpySize) {
+    auto keyLen = key.size();
+    cpySize = 0;
+    char c;
+    static int lastChar = -1;
+
+    if (lastChar != -1) {
+      while (escbuf.size()) {
+        buf[cpySize++] = escbuf.front();
+        escbuf.pop_front();
+        if (cpySize >= bufSize) {
+          return false;
+        }
+      }
+
+      buf[cpySize++] = static_cast<char>(lastChar);
+      lastChar = -1;
+      if (cpySize >= bufSize) {
+        return false;
+      }
+    }
+
+    while (m_readPtr++ < m_length) {
+      m_stream->get(c);
+      if (c == key[escbuf.size()]) {
+        escbuf.push_back(c);
+        if (escbuf.size() >= keyLen) {
+          escbuf.clear();
+          return true;
+        }
+      }
+      else {
+        while (escbuf.size()) {
+          buf[cpySize++] = escbuf.front();
+          escbuf.pop_front();
+          if (cpySize >= bufSize) {
+            lastChar = static_cast<unsigned char>(c);
+            return false;
+          }
+        }
+        buf[cpySize++] = c;
+        if (cpySize >= bufSize) {
+          return false;
+        }
+      }
+    }
+
+    return false;
+  }
+
+public:
+
+  int SeekToNextSection() {
+    return SeekUntil(*m_boundary);
+  }
+
+  int ReadHeader(string& header) {
+    char buf[2048];
+    size_t size;
+    ReadUntil(buf, 2048, "\r\n\r\n", size);
+    if (size >= 2048) {
+      throw runtime_error("error: header is too large.");
+    }
+
+    buf[size] = 0;
+    header = buf;
+    return size;
+  }
+
+  size_t ReadTextBody(string& body) {
+    char buf[2048];
+    size_t readBytes = 0;
+    size_t size = 0;
+
+    while (!ReadUntil(buf, 2048, *m_boundary, size)) {
+      body.append(buf, size);
+      readBytes += size;
+    }
+    body.append(buf, size);
+    readBytes += size;
+
+    return readBytes;
+  }
+
+  bool ReadFileBody(char* buf, size_t bufSize, size_t& readSize) {
+    return ReadUntil(buf, bufSize, *m_boundary, readSize);
+  }
+};
